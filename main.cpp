4ccd2641cf1a87540bcd57475c5f8872428bc07c
gockdd1677@gmail.com <gockdd1677@gmail.com>
2021-05-06 16:16:20 +0900

    :)

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my.h"
+int main()
+{
+    std::cout<<"Hello World!\n";
+    std::cout<<"Calling up myfunc:"<<myfunc(3)<<std::endl;
+    return 0;
+}
---
+++ my.cpp
@@ -0,0 +1,7 @@
+#include "my.h"
+
+int myfunc(int val)
+
+{
+    return val + 1;
+}
